80716e56a22f53085cbb1e10cc06522bf920e02b
bennett <xiaowen2714@139.com>
2025-12-30 21:45:29 +0800

    chore: 使用torch_data构造数据集；对时间维度进行特征归一化；删除多余padding操作；y_truth归一化

--- utils/data_prepare.py
+++ utils/data_prepare.py
@@ -11,19 +11,9 @@ from torch.utils.data.distributed import DistributedSampler
 from .batch import Batch
 from .list_data import ListDataset
 from .utils import StandardScaler, log_string
-
+import torch.utils.data as torch_data
 
 # ! X shape: (B, T, N, C)
-
-def collator(indices):
-    """Collate function for batching data"""
-    feature_name = {'x': 'float', 'y': 'float'}
-    batch = Batch(feature_name, pad_item=None, pad_max_len=None)
-    for item in indices:
-        batch.append(copy.deepcopy(item))
-    batch.padding()
-    return batch
-
 
 def seq2instance(data, P, Q):
     num_step, num_nodes, dims = data.shape
@@ -43,7 +33,6 @@ def get_dataloaders(args, log, world_size=1, rank=0):
     data = np.expand_dims(data, axis=-1)
 
     L, N, F = data.shape
-
     # feature_list 1 × T × N × D
     feature_list = [data]
 
@@ -52,17 +41,21 @@ def get_dataloaders(args, log, world_size=1, rank=0):
     time_ind = [i % 288 for i in range(data.shape[0])]
     time_ind = np.array(time_ind)
     time_in_day = np.tile(time_ind, [1, N, 1]).transpose((2, 1, 0))
+    # 需要进行归一化
+    time_in_day_norm = time_in_day / args.T  # 归一化到 [0, 1)
 
     # time_in_day 16992 × 307 × 1
-    feature_list.append(time_in_day)
+    feature_list.append(time_in_day_norm)
 
     # numerical day_in_week
     day_in_week = [(i // 288) % 7 for i in range(data.shape[0])]
     day_in_week = np.array(day_in_week)
     day_in_week = np.tile(day_in_week, [1, N, 1]).transpose((2, 1, 0))
+    # 归一化
+    day_in_week_norm = day_in_week / 7.0 # 归一化到 [0, 1)
     # day_in_week 16992 × 307 × 1
     # feature_list 3 × T × N × D
-    feature_list.append(day_in_week)
+    feature_list.append(day_in_week_norm)
 
     # generate_data
     # data 16992 × 307 × 3
@@ -70,7 +63,6 @@ def get_dataloaders(args, log, world_size=1, rank=0):
     x, y = seq2instance(data, args.P, args.Q)
 
     # split train/val/test
-    # TODO 缺少padding处理
     num_step = data.shape[0]
     train_steps = round(args.train_ratio * num_step)
     test_steps = round(args.test_ratio * num_step)
@@ -87,9 +79,9 @@ def get_dataloaders(args, log, world_size=1, rank=0):
     valX, valY = x[train_steps: train_steps + val_steps], y[train_steps: train_steps + val_steps]
     testX, testY = x[-test_steps:], y[-test_steps:]
 
-    print('trainX: %s\ttrainY: %s' % (trainX.shape, trainY.shape))
-    print('valX: %s\tvalY: %s' % (valX.shape, valY.shape))
-    print('testX: %s\ttestY: %s' % (testX.shape, testY.shape))
+    # print('trainX: %s\ttrainY: %s' % (trainX.shape, trainY.shape))
+    # print('valX: %s\tvalY: %s' % (valX.shape, valY.shape))
+    # print('testX: %s\ttestY: %s' % (testX.shape, testY.shape))
 
     log_string(log, "train\t" + "x: " + str(trainX.shape) + ", y: " + str(trainX.shape))
     log_string(log, "eval\t" + "x: " + str(valX.shape) + ", y: " + str(valY.shape))
@@ -100,42 +92,16 @@ def get_dataloaders(args, log, world_size=1, rank=0):
     scaler = StandardScaler(mean=trainX[..., 0].mean(), std=trainX[..., 0].std())
 
     trainX[..., 0] = scaler.transform(trainX[..., 0])
-    # trainY[..., 0] = scaler.transform(trainY[..., 0])
+    trainY[..., 0] = scaler.transform(trainY[..., 0])
     valX[..., 0] = scaler.transform(valX[..., 0])
-    # valY[..., 0] = scaler.transform(valY[..., 0])
+    valY[..., 0] = scaler.transform(valY[..., 0])
     testX[..., 0] = scaler.transform(testX[..., 0])
     # testY[..., 0] = scaler.transform(testY[..., 0])
 
-    train_data = list(zip(trainX, trainY))
-    eval_data = list(zip(valX, valY))
-    test_data = list(zip(testX, testY))
-
-    # 修复: 正确处理元组列表的padding
-    num_padding = (args.batch_size - (len(train_data) % args.batch_size)) % args.batch_size
-    if num_padding > 0:
-        train_data.extend([train_data[-1]] * num_padding)
-
-    num_padding = (args.batch_size - (len(eval_data) % args.batch_size)) % args.batch_size
-    if num_padding > 0:
-        eval_data.extend([eval_data[-1]] * num_padding)
-
-    num_padding = (args.batch_size - (len(test_data) % args.batch_size)) % args.batch_size
-    if num_padding > 0:
-        test_data.extend([test_data[-1]] * num_padding)
-
-    train_dataset = ListDataset(train_data)
-    eval_dataset = ListDataset(eval_data)
-    test_dataset = ListDataset(test_data)
-
-    # trainset = torch.utils.data.TensorDataset(
-    #     torch.FloatTensor(trainX), torch.FloatTensor(trainY)
-    # )
-    # valset = torch.utils.data.TensorDataset(
-    #     torch.FloatTensor(valX), torch.FloatTensor(valY)
-    # )
-    # testset = torch.utils.data.TensorDataset(
-    #     torch.FloatTensor(testX), torch.FloatTensor(testY)
-    # )
+    # 转换为 Tensor
+    train_dataset = torch_data.TensorDataset(torch.FloatTensor(trainX), torch.FloatTensor(trainY))
+    eval_dataset = torch_data.TensorDataset(torch.FloatTensor(valX), torch.FloatTensor(valY))
+    test_dataset = torch_data.TensorDataset(torch.FloatTensor(testX), torch.FloatTensor(testY))
 
     # Create distributed samplers for multi-GPU training
     if world_size > 1:
@@ -171,7 +137,7 @@ def get_dataloaders(args, log, world_size=1, rank=0):
         batch_size=args.batch_size,
         sampler=train_sampler,
         shuffle=(train_sampler is None),  # Only shuffle if not using sampler
-        collate_fn=collator,
+        collate_fn=None,
         num_workers=num_workers,
         pin_memory=True,
         persistent_workers=True if num_workers > 0 else False,
@@ -182,7 +148,7 @@ def get_dataloaders(args, log, world_size=1, rank=0):
         batch_size=args.batch_size,
         sampler=val_sampler,
         shuffle=False,
-        collate_fn=collator,
+        collate_fn=None,
         num_workers=num_workers,
         pin_memory=True,
         persistent_workers=True if num_workers > 0 else False,
@@ -193,7 +159,7 @@ def get_dataloaders(args, log, world_size=1, rank=0):
         batch_size=args.batch_size,
         sampler=test_sampler,
         shuffle=False,
-        collate_fn=collator,
+        collate_fn=None,
         num_workers=num_workers,
         pin_memory=True,
         persistent_workers=True if num_workers > 0 else False,
